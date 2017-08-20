025b64eb9b9fcfef7e87ee9cc36b0e9bdda79452
moemursi <jemkarry@gmail.com>
2017-08-20 12:13:45 -0400

    Added Array of dice images , make the dice rolls

--- app/src/main/java/com/mamapps/dicer/MainActivity.java
+++ app/src/main/java/com/mamapps/dicer/MainActivity.java
@@ -18,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button rollButton = (Button) findViewById(R.id.roll_button);
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final int [] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Dicee","Button pressed !!!!! ");
                 Random randomNum = new Random();
                 int num = randomNum.nextInt(6);
+                int num2 = randomNum.nextInt(6);
                 Log.d("dicee", "THE RANDOM NUMBER IS "+num);
+
+                leftDice.setImageResource(diceArray[num]);
+                rightDice.setImageResource(diceArray[num2]);
+
             }
         });
     }
